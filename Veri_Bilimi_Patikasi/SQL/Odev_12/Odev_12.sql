5496c2e56a1002a3e7ff8a8ecb450d98e3be28f9
Yavuz BALI <84620286+YavuzBALI@users.noreply.github.com>
2021-11-16 15:07:18 +0300

    Update Odev_12.sql

--- Veri_Bilimi_Patikasi/SQL/Odev_12/Odev_12.sql
+++ Veri_Bilimi_Patikasi/SQL/Odev_12/Odev_12.sql
@@ -1,10 +1,31 @@
 -- film tablosunda film uzunluğu length sütununda gösterilmektedir. Uzunluğu ortalama film uzunluğundan fazla kaç tane film vardır?
-
+SELECT COUNT(*) FROM film
+WHERE length >
+(
+SELECT avg(length) FROM film
+);
 
 -- film tablosunda en yüksek rental_rate değerine sahip kaç tane film vardır?
-
+SELECT COUNT(*) FROM film
+WHERE rental_rate =
+(
+SELECT max(rental_rate) FROM film
+);
 
 -- film tablosunda en düşük rental_rate ve en düşün replacement_cost değerlerine sahip filmleri sıralayınız.
+SELECT COUNT(*) FROM film
+WHERE rental_rate =
+(
+SELECT min(rental_rate) FROM film
+) 
 
+AND replacement_cost =
+(
+SELECT min(replacement_cost) FROM film
+);
 
 -- payment tablosunda en fazla sayıda alışveriş yapan müşterileri(customer) sıralayınız.
+
+SELECT customer_id,COUNT(*) FROM payment
+GROUP BY customer_id 
+ORDER BY COUNT(*) DESC;
